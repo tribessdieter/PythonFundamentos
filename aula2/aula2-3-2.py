fca3b2aaf9069c8285c38995dfb6bb2543e95940
Dieter Nivaldo Tribess <dieter_nivaldo@hotmail.com>
2021-08-28 18:21:49 -0300

    tupla

--- aula2/aula2-3-2.py
+++ aula2/aula2-3-2.py
@@ -3,8 +3,8 @@
 # ASSUNTO :  Tuplas 
 # DATA : 2021-08-28-
 
-
+# tuplass são criadas com parenteses e não podem mais ser alteradas
 bebidas = ('Coca-Cola ', 'SKOL','Cachaça','Áqua')
-print(bebidas[0])
-print(bebidas[2:])
+print(bebidas[0]) # imprimindo primeiro elemento da tupla
+print(bebidas[2:]) # imprimindo parte da tupla
 
