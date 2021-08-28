0301c8932a352cc05edf2aa6c475878e1ee332c1
Dieter Nivaldo Tribess <dieter_nivaldo@hotmail.com>
2021-08-28 14:45:15 -0300

    adicionando arquivos da aula 2

---
+++ aula1/a2-1.py
@@ -0,0 +1,29 @@
+# curso : pyton fundamentos proway
+# aula : dia 2 - parte 1
+# assunto : if ,else, elif
+# data 2021-08-28
+
+idade = -18
+
+# maior de idade > 18
+if(idade >= 18):
+    print('maior de 18')
+else :
+    if(idade >0):
+       print('menor que 18')
+    else :
+      print('Idade inválida')       
+
+
+
+
+
+
+
+# maior de idade > 18
+if(idade >= 18):
+    print('maior de 18')
+elif(idade >0):
+    print('menor que 18')
+else :
+    print('Idade inválida')       
